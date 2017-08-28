2474c8ff142d386ae955a0ebecda1e9ce3afecab
Gar <gar@lonelydatu.com>
2017-08-27 23:09:25 -0400

    bg

--- dev/std_300x250/_bundled/log-free.js
+++ dev/std_300x250/_bundled/log-free.js
@@ -34,19 +34,20 @@ function start() {
 
 	tl.add('f1');
 	tl.from(dom.phoneMain, .5, { y: 190 }, 'f1');
-	tl.from("#t1", .5, { opacity: 0, y: '+=80' }, 'f1');
+	tl.from("#t1", .5, { opacity: 0, y: '+=130' }, 'f1');
 
-	tl.add('f2', '+=.3');
+	tl.add('f2', '+=.6');
 	tl.from("#t1", .5, { x: 125 }, 'f2');
 	tl.from("#t1 span", .5, { clip: 'rect(0px,0px,51px,0px)' }, 'f2');
-	tl.add(rotate(0, 5), 'f2');
+	tl.add(rotate(0, 9), 'f2');
+	tl.to(dom.screen, .9, { opacity: 1 });
 	// tl.to(dom.phoneMain, .3, {y:31}, 'f2')
 
 	tl.add('f3', '+=1');
 	tl.to('#t1', .3, { opacity: 0 }, 'f3');
-	tl.from('#t2', .3, { opacity: 0 }, 'f3+=.3');
+	tl.from('#t2', .3, { opacity: 0, y: '+=35' }, 'f3+=.3');
 
-	tl.add(rotate(5, 9), 'f3+=.3');
+	// tl.add(rotate(5, 9), 'f3+=.3')
 	tl.from('#t3', .3, { opacity: 0 }, '+=1');
 	tl.to('#t3', .3, { opacity: 0 }, '+=1');
 	tl.from('#t4', .3, { opacity: 0 });
@@ -54,7 +55,8 @@ function start() {
 	tl.add('end', '+=1');
 	tl.to(['#t2', '#t4'], .3, { opacity: 0, y: '-=40' }, 'end');
 	tl.to(dom.phoneMain, .4, { y: -46 }, 'end');
-	tl.to(dom.screen, .6, { opacity: 1 });
+	tl.from(".logos", .4, { opacity: 0 }, 'end');
+
 	tl.from('#screen p', .3, { opacity: 0 }, '+=.6');
 
 	tl.from(['.end'], .3, { opacity: 0 }, '+=.3');
@@ -66,15 +68,13 @@ function rotate(from, to) {
 	for (var i = from; i <= to; i++) {
 		var x = -i * width;
 		void 0;
-		tlRotate.set(dom.phone, { x: x }, '+=.09');
+		tlRotate.set(dom.phone, { x: x }, '+=.07');
 	}
 
 	return tlRotate;
 }
 
 start();
-
-void 0;
 
 },{}]},{},[1])
 
--- dev/std_300x250/_js/main.js
+++ dev/std_300x250/_js/main.js
@@ -31,21 +31,23 @@ const dom = {
 function start(){
 	tl.set('.frame1', {opacity: 1})
 
+
 	tl.add('f1')
 	tl.from(dom.phoneMain, .5, {y:190}, 'f1')
-	tl.from("#t1", .5, {opacity: 0, y:'+=80'}, 'f1')
+	tl.from("#t1", .5, {opacity: 0, y:'+=130'}, 'f1')
 	
-	tl.add('f2', '+=.3')
+	tl.add('f2', '+=.6')
 	tl.from("#t1", .5, {x: 125}, 'f2')
 	tl.from("#t1 span", .5, {clip:`rect(0px,0px,51px,0px)`}, 'f2')
-	tl.add(rotate(0, 5), 'f2')
+	tl.add(rotate(0, 9), 'f2')
+	tl.to(dom.screen, .9, {opacity:1})
 	// tl.to(dom.phoneMain, .3, {y:31}, 'f2')
 
 	tl.add('f3', '+=1')
 	tl.to('#t1', .3, {opacity:0}, 'f3')
-	tl.from('#t2', .3, {opacity:0}, 'f3+=.3')
+	tl.from('#t2', .3, {opacity:0, y:'+=35'}, 'f3+=.3')
 
-	tl.add(rotate(5, 9), 'f3+=.3')
+	// tl.add(rotate(5, 9), 'f3+=.3')
 	tl.from('#t3', .3, {opacity:0}, '+=1')
 	tl.to('#t3', .3, {opacity:0}, '+=1')
 	tl.from('#t4', .3, {opacity:0})
@@ -53,7 +55,8 @@ function start(){
 	tl.add('end', '+=1')
 	tl.to(['#t2', '#t4'], .3, {opacity:0, y:'-=40'}, 'end')
 	tl.to(dom.phoneMain, .4, {y:-46}, 'end')
-	tl.to(dom.screen, .6, {opacity:1})
+	tl.from(".logos", .4, {opacity:0}, 'end')
+	
 	tl.from('#screen p', .3, {opacity:0}, '+=.6')
 
 	tl.from(['.end'], .3, {opacity: 0}, '+=.3')
@@ -67,7 +70,7 @@ function rotate(from, to){
 	for(let i=from;i<=to;i++){
 		const x = -i*width
 		console.log(i)
-		tlRotate.set(dom.phone, {x:x}, '+=.09')
+		tlRotate.set(dom.phone, {x:x}, '+=.07')
 	}
 
 	return tlRotate
@@ -76,4 +79,3 @@ function rotate(from, to){
 
 start()
 
-console.log('sldkfj')
